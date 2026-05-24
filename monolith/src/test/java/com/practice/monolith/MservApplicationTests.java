8f99dabb807b5aea5e803b8449a7969bd83abac9
Shubham <vishu.bhardwaz2001@gmail.com>
2026-05-24 17:51:28 +0530

    Pushing monolith app v1

---
+++ monolith/src/test/java/com/practice/monolith/MservApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.monolith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
